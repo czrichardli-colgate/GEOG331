2ecc90ab5a83b3a238e66b89a32688b1e7404564
czrichardli-colgate <cli2@colgate.edu>
2025-11-20 12:00:59 -0500

    As of Nov 20.

--- spatialAnalysis.R
+++ spatialAnalysis.R
@@ -47,3 +47,13 @@ mad_buf <- buffer(mad_dec, width = 1000, singlesided = T)
 # create a plot to look at our Madison County data
 plot(mad_dec, col = "red")
 plot(mad_buf, col = "yellow", add = T)
+
+# calculate ndvi for dec lands
+mad_dec$ndvi_in <- zonal(ndvi, mad_dec, fun = "mean")
+
+# calculate ndvi for the buffer outside dec lands
+mad_dec$ndvi_out <- zonal(ndvi, mad_buf, fun = "mean")
+
+# calculate the difference to see if DEC lands are more productive
+mad_dec$ndvi_dif <- mad_dec$ndvi_in-mad_dec$ndvi_out
+
